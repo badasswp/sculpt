c85416c8ffcea1d83653a6a0e58f8149e1f9c6fd
badasswp <badasswpdev@gmail.com>
2025-02-24 23:24:02 +0100

    chore: cli entry point

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+/**
+ * sculpt-cli
+ *
+ * A CLI tool for sculpting WP plugins quickly.
+ *
+ * @author badasswp <https://github.com/badasswp>
+ */
+
+import cli from './utils/cli.js';
+import init from './utils/init.js';
+import log from './utils/log.js';
+
+const { flags, input, showHelp } = cli;
+const { clear, debug } = flags;
+
+(async () => {
+  await init({ clear });
+  debug && log(flags);
+  input.includes(`help`) && showHelp(0);
+})();
