9ad8707638c5c7dd9b7d7dd82c14f736b65be9cc
badasswp <badasswpdev@gmail.com>
2025-02-26 12:08:56 +0100

    chore: add plugin prompts

--- src/prompts.js
+++ src/prompts.js
@@ -1,7 +1,8 @@
 /**
- * getPostPrompts
+ * Get Post Prompts.
  *
- * Returns an object with prompts for generating a Custom Post Type.
+ * This function returns an object with prompts for
+ * generating a Custom Post Type.
  *
  * @returns {Object}
  */
@@ -17,4 +18,21 @@ export const getPostPrompts = () => ({
 	showInRest: 'Show in REST: ',
 	restBase: 'REST Base: ',
 	restController: 'REST Controller Class: '
+});
+
+/**
+ * Get Plugin Prompts.
+ *
+ * This function returns an object with prompts for
+ * generating a Plugin.
+ *
+ * @returns {Object}
+ */
+export const getPluginPrompts = () => ({
+	name: 'Plugin name: ',
+	description: 'Description: ',
+	author: 'Author: ',
+	domain: 'Text domain: ',
+	package: 'Package: ',
+	namespace: 'Namespace: ',
 });
