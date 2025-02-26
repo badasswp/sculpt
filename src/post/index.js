37047e42a25873a5acfb887b78d057f6aeb6e2d9
badasswp <badasswpdev@gmail.com>
2025-02-26 12:01:43 +0100

    chore: modify Sculpt Post feat

--- src/post/index.js
+++ src/post/index.js
@@ -6,10 +6,12 @@ import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
 
 /**
- * sculptPost
+ * Scult Post.
  *
- * Prompts the user for information to generate a Custom Post Type.
+ * This function prompts the user for information and uses same
+ * to generate a Custom Post Type.
  *
+ * @since 1.0.0
  * @returns {Promise<void>}
  */
 const sculptPost = async () => {
@@ -31,36 +33,38 @@ const sculptPost = async () => {
 		return;
 	}
 
-	try {
-		// Get the script's absolute directory.
-		const __filename = fileURLToPath(import.meta.url);
-		const __dirname = path.dirname(__filename);
+	createPost(cpt);
+};
 
-		// Read the template file from the script's location.
-		const templatePath = path.join(
-			__dirname,
-			'../../repo/Posts',
-			'Post.php'
-		);
-		const templateContent = await fs.readFile(templatePath, 'utf-8');
+/**
+ * Create Post.
+ *
+ * This function creates a new custom post type file based on the provided
+ * custom post type properties.
+ *
+ * @since 1.0.0
+ *
+ * @param {Object} cpt
+ * @returns {Promise<void>}
+ */
+const createPost = async cpt => {
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
 
-		// Replace text verbiage with post type properties.
-		const newContent = templateContent
-			.replace(/\bSculpt\b/g, cpt.singular)
-			.replace(/\bcpt\b/g, cpt.name.toLowerCase())
-			.replace(/\bSingular_Label\b/g, cpt.singular)
-			.replace(/\bPlural_Label\b/g, cpt.plural)
-			.replace(/\btext_domain\b/g, 'obo');
+	const templatePath = path.join(__dirname, '../../repo/Posts', 'Post.php');
+	const templateContent = await fs.readFile(templatePath, 'utf-8');
 
-		// Define new file path.
-		const newFilePath = path.join(process.cwd(), `${cpt.singular}.php`);
+	const newContent = templateContent
+		.replace(/\bSculpt\b/g, cpt.singular)
+		.replace(/\bcpt\b/g, cpt.name.toLowerCase())
+		.replace(/\bSingular_Label\b/g, cpt.singular)
+		.replace(/\bPlural_Label\b/g, cpt.plural)
+		.replace(/\btext_domain\b/g, 'obo');
 
-		// Write the modified content to the new file.
-		await fs.writeFile(newFilePath, newContent, 'utf-8');
-		console.log(`Custom post type created: ${newFilePath}`);
-	} catch (error) {
-		console.error('Error creating file:', error);
-	}
+	const newFilePath = path.join(process.cwd(), `${cpt.singular}.php`);
+	await fs.writeFile(newFilePath, newContent, 'utf-8');
+
+	console.log(`Custom post type created: ${newFilePath}`);
 };
 
 export default sculptPost;
