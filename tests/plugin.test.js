6669230e53837463c8551346d0ce1e18d3f3a89c
badasswp <badasswpdev@gmail.com>
2025-03-08 23:52:28 +0100

    test: update unit tests

--- tests/plugin.test.js
+++ tests/plugin.test.js
@@ -1,29 +1,35 @@
+const getSanitizedText = jest.fn(text => {
+	return text
+		.split(' ')
+		.filter(item => /[a-zA-Z0-9]+$/.test(item))
+		.join(' ')
+		.replace(/[^a-zA-Z0-9\s]/g, '');
+});
+
 describe('getSanitizedText', () => {
 	it('should return a sanitized string', () => {
-		const getSanitizedText = jest.fn(text => {
-			return text
-				.split(' ')
-				.filter(item => /[a-zA-Z0-9]+$/.test(item))
-				.join(' ')
-				.replace(/[^a-zA-Z0-9\s]/g, '');
-		});
-
 		const expected = getSanitizedText('Search & Replace');
 
 		expect(expected).toBe('Search Replace');
 	});
 });
 
-describe('getSlug', () => {
-	it('should return a slug', () => {
-		const getSanitizedText = jest.fn(text => {
-			return text
-				.split(' ')
-				.filter(item => /[a-zA-Z0-9]+$/.test(item))
-				.join(' ')
-				.replace(/[^a-zA-Z0-9\s]/g, '');
+describe('getDescription', () => {
+	it('should return plugin description', () => {
+		const getDescription = jest.fn(name => {
+			return `The ${name} plugin is a WordPress plugin that does amazing things.`;
 		});
 
+		const expected = getDescription('Search & Replace');
+
+		expect(expected).toBe(
+			'The Search & Replace plugin is a WordPress plugin that does amazing things.'
+		);
+	});
+});
+
+describe('getSlug', () => {
+	it('should return a slug', () => {
 		const getSlug = jest.fn(name => {
 			return getSanitizedText(name).toLowerCase().replace(/\s/g, '-');
 		});
@@ -31,5 +37,22 @@ describe('getSlug', () => {
 		const expected = getSlug('Search & Replace');
 
 		expect(expected).toBe('search-replace');
+	});
+});
+
+describe('getNamespace', () => {
+	it('should return a namespace', () => {
+		const getNamespace = jest.fn((name) => {
+			return getSanitizedText(name)
+				.split(' ')
+				.map(item => {
+					return `${item.charAt(0).toUpperCase()}${item.slice(1)}`;
+				})
+				.join('');
+		});
+
+		const expected = getNamespace('Easy Gutenberg Blocks');
+
+		expect(expected).toBe('EasyGutenbergBlocks');
 	});
 });
