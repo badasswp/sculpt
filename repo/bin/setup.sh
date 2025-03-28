14c1f484c05b3396e095b49c916fb0b3fc305336
badasswp <badasswpdev@gmail.com>
2025-03-28 17:39:08 +0100

    chore: update wp-env script

--- repo/bin/setup.sh
+++ repo/bin/setup.sh
@@ -3,4 +3,4 @@
 wp-env run cli wp theme activate twentytwentythree
 wp-env run cli wp rewrite structure /%postname%
 wp-env run cli wp option update blogname "WordPress Site"
-wp-env run cli wp option update blogdescription "A WordPress site for plugin devlopment"
+wp-env run cli wp option update blogdescription "A WordPress site for plugin devlopment by Sculpt."
