69a6d5c75e1f9c66f9f28b25970f15aad863b792
badasswp <badasswpdev@gmail.com>
2025-03-28 17:04:33 +0100

    feat: add setup script

---
+++ repo/bin/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wp-env run cli wp theme activate twentytwentythree
+wp-env run cli wp rewrite structure /%postname%
+wp-env run cli wp option update blogname "WordPress Site"
+wp-env run cli wp option update blogdescription "A WordPress site for plugin devlopment"
