c909b08d18620e04d99fabb9e34f2107fcab6a3d
badasswp <badasswpdev@gmail.com>
2025-05-28 14:26:13 +0100

    fix: typo in placeholder

--- src/meta/index.js
+++ src/meta/index.js
@@ -179,7 +179,7 @@ const createMeta = async props => {
 	);
 
 	await fs.writeFile(newFilePath, fileContent, 'utf-8');
-	console.log(`New Meta created: ${singular}\n`);
+	console.log(`New Meta created: ${getNamespace(name)}\n`);
 };
 
 /**
